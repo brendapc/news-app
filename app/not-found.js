f9d1fa6fe2edc383590f9e354693d8cfc5a09c2c
Brenda Pereira <brendacamara@outlook.com>
2024-06-07 21:47:15 -0300

    feat: add not found pages

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+  return (
+    <div id="error">
+      <h1>Page Not Found!</h1>
+      <p>Sorry, the page you are looking for could not be found.</p>
+    </div>
+  );
+}

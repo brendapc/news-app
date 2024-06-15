c050d4ab073af3aefdceb30be34f4dc64e46631f
Brenda Pereira <brendacamara@outlook.com>
2024-06-14 21:30:10 -0300

    paralel routes - archive news

---
+++ app/archive/@latest/default.js
@@ -0,0 +1,14 @@
+import NewsList from "@/components/news-list"
+import { getLatestNews } from "@/lib/news"
+
+/* all paralels route should support the same nested routes, but for latest page it does not makes sense to support a [year] nested route, do it need a `default` (name reserved) to have where to land */
+export default function LatestNewsPage() {
+  const latestNews = getLatestNews()
+
+  return (
+    <>
+      <h2>Latest News</h2>
+      <NewsList news={latestNews} />
+    </>
+  )
+}
